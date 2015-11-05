4afdd460f371f9e16929c731f31ebb55d6851f85
Max Thomas <maxamadeothomas@gmail.com>
2015-11-05 15:08:26 -0500

    goats fade in and out - only start button starts it

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 $(document).ready(function(){
+	console.log('linked!');
+	var $allGoats = $('.button');
+	var $goatOne = $('#goatOne');
+	var $goatTwo = $('#goatTwo');
+	var $goatThree = $('#goatThree');
+	var $goatFour = $('#goatFour');
+	var $start= $('#start')
+	var removeGoat = function(select,place){
+		setTimeout(function(){
+			select.removeClass(place)},[500]
+		)
+	}
+	var goatSelector = function(event){
+		if (event.target.id==='goatOne'){
+			$goatOne.addClass('activateGoatOne')
+			removeGoat($goatOne,'activateGoatOne')
+		} else if (event.target.id==='goatTwo'){
+			$goatTwo.addClass('activateGoatTwo')
+			removeGoat($goatTwo,'activateGoatTwo')
+		} else if (event.target.id==='goatThree'){
+			$goatThree.addClass('activateGoatThree')
+			removeGoat($goatThree,'activateGoatThree')
+		} else if (event.target.id==='goatFour'){
+			$goatFour.addClass('activateGoatFour')
+			removeGoat($goatFour,'activateGoatFour')
+		}
+
+	}
+
+	var startGame = function(event){
+		$allGoats.on('click',goatSelector)		
+	}
+
+	$start.on('click',startGame)
 
 
 
 
 
-
-
-
-	
 })
