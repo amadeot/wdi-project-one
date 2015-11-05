8a922e87cf3998ff2ff399d0e998771c5c898e22
Max Thomas <maxamadeothomas@gmail.com>
2015-11-05 16:06:16 -0500

    DRYed existing javascript

--- main.js
+++ main.js
@@ -6,32 +6,37 @@ $(document).ready(function(){
 	var $goatThree = $('#goatThree');
 	var $goatFour = $('#goatFour');
 	var $start= $('#start')
-	var removeGoat = function(select,place){
-		setTimeout(function(){
-			select.removeClass(place)},[500]
-		)
-	}
-	var goatSelector = function(event){
-		if (event.target.id==='goatOne'){
-			$goatOne.addClass('activateGoatOne')
-			removeGoat($goatOne,'activateGoatOne')
-		} else if (event.target.id==='goatTwo'){
-			$goatTwo.addClass('activateGoatTwo')
-			removeGoat($goatTwo,'activateGoatTwo')
-		} else if (event.target.id==='goatThree'){
-			$goatThree.addClass('activateGoatThree')
-			removeGoat($goatThree,'activateGoatThree')
-		} else if (event.target.id==='goatFour'){
-			$goatFour.addClass('activateGoatFour')
-			removeGoat($goatFour,'activateGoatFour')
+
+	var goatPen = [$goatOne,$goatTwo,$goatThree,$goatFour]
+	var usedGoats = []
+
+	var startGame = function(event){	
+		var addRemoveGoat=function(select,place){
+			var addGoat = function(select,place){
+			select.addClass(place)
 		}
-
-	}
-
-	var startGame = function(event){
-		$allGoats.on('click',goatSelector)		
-	}
-
+			var removeGoat = function(select,place){
+				setTimeout(function(){
+					select.removeClass(place)},[500]
+				)
+			}
+			addGoat(select,place)
+			removeGoat(select,place)
+		}
+		var goatSelector = function(event){
+			if (event.target.id==='goatOne'){
+				addRemoveGoat($goatOne,'activateGoatOne')
+			} else if (event.target.id==='goatTwo'){
+				addRemoveGoat($goatTwo,'activateGoatTwo')
+			} else if (event.target.id==='goatThree'){
+				addRemoveGoat($goatThree,'activateGoatThree')
+			} else if (event.target.id==='goatFour'){
+				addRemoveGoat($goatFour,'activateGoatFour')
+			}
+		}
+		$allGoats.on('click',goatSelector)
+		}
+		for(var i=0;)
 	$start.on('click',startGame)
 
 
