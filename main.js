ec631f2e618b3d4633f10df9279d60b3487bf72a
Max Thomas <maxamadeothomas@gmail.com>
2015-11-06 13:32:18 -0500

    recognize first goat

--- main.js
+++ main.js
@@ -1,63 +1,67 @@
+$(document).ready(function() {
 console.log('linked!');
-$allGoats = $('.button');
+$allGoats = $('.goatButton');
 $goatOne = $('#goatOne');
 $goatTwo = $('#goatTwo');
 $goatThree = $('#goatThree');
 $goatFour = $('#goatFour');
-$start = $('#start')
+$start = $('#start');
 
-$goatList = [$goatOne,$goatTwo,$goatThree,$goatFour]
-$goatPen = []
+$goatList = [$goatOne,$goatTwo,$goatThree,$goatFour];
+$goatPen = [];
 
 var startGame = function(event){	
 	var addRemoveGoat=function(select,place){
 		var addGoat = function(select,place){
-		select.addClass(place)
+		select.addClass(place);
 	}
 		var removeGoat = function(select,place){
 			setTimeout(function(){
 				select.removeClass(place)},[500]
-			)
+			);
 		}
 		addGoat(select,place)
 		removeGoat(select,place)
 	}
 	var goatSelector = function(event){
 		if (event.target.id==='goatOne'){
-			addRemoveGoat($goatOne,'activateGoatOne')
+			addRemoveGoat($goatOne,'activateGoatOne');
 		} else if (event.target.id==='goatTwo'){
-			addRemoveGoat($goatTwo,'activateGoatTwo')
+			addRemoveGoat($goatTwo,'activateGoatTwo');
 		} else if (event.target.id==='goatThree'){
-			addRemoveGoat($goatThree,'activateGoatThree')
+			addRemoveGoat($goatThree,'activateGoatThree');
 		} else if (event.target.id==='goatFour'){
-			addRemoveGoat($goatFour,'activateGoatFour')
+			addRemoveGoat($goatFour,'activateGoatFour');
 		}
 	}	
-	$allGoats.on('click',goatSelector)
-	}
+	$allGoats.on('click',goatSelector);
 	var addRandomGoatToPen = function(){
-		var i=Math.floor(Math.random()*4)
-		$goatPen.push($goatList[i])
+		var i=Math.floor(Math.random()*4);
+		$goatPen.push($goatList[i]);
 	}
-	addRandomGoatToPen()
-	$firstGoat = $goatPen[0]
-	console.log($firstGoat)
-	var CompGoatSelector = function($firstGoat){
-		console.log('this works')
-		if ($firstGoat==='goatOne'){
-			console.log('please work 1')
-			addRemoveGoat($goatOne,'activateGoatOne')
-		} else if ($firstGoat==='goatTwo'){
-			console.log('please work 2')
-			addRemoveGoat($goatTwo,'activateGoatTwo')
-		} else if ($firstGoat==='goatThree'){
-			console.log('please work 3')
-			addRemoveGoat($goatThree,'activateGoatThree')
-		} else if ($firstGoat==='goatFour'){
-			console.log('please work 4')
-			addRemoveGoat($goatFour,'activateGoatFour')
+	addRandomGoatToPen();
+	$firstGoat = $goatPen[0];
+	var CompGoatSelector = function($firstGoat){  
+		if ($firstGoat.selector==="#goatOne"){
+			addRemoveGoat($goatOne,'activateGoatOne');
+		} else if ($firstGoat.selector==="#goatTwo"){
+			addRemoveGoat($goatTwo,'activateGoatTwo');
+		} else if ($firstGoat.selector==="#goatThree"){
+			addRemoveGoat($goatThree,'activateGoatThree');
+		} else if ($firstGoat.selector==="#goatFour"){
+			addRemoveGoat($goatFour,'activateGoatFour');
+		};
+	};
+	CompGoatSelector($firstGoat);
+
+	var clickRightGoat = function(event){
+		if(event.target.id===$firstGoat.selector.replace('#','')){
+			addRandomGoatToPen()
+		} else {
+			alert("WRONG GOAT TRY AGAIN")
 		}
 	}
-	CompGoatSelector($firstGoat)
-	
-$start.on('click',startGame)
+	$allGoats.on('click',clickRightGoat)
+};
+$start.on('click',startGame);
+});
