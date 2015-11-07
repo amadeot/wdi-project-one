9895b54d86d6cac76a622800e6286a81512e5b32
Max Thomas <maxamadeothomas@gmail.com>
2015-11-07 17:59:24 -0500

    testing out using non-random selector

--- main.js
+++ main.js
@@ -13,26 +13,30 @@ $(document).ready(function() {
 	counter=0;
 	computerClick=0;
 	humanClick=0;
+	goatNumber=0;
 
 	var startGame = function(event){	
 		var addRemoveGoat=function(select,place){
 			var addGoat = function(select,place){
 			select.addClass(place);
-		}
+		};
 			var removeGoat = function(select,place){
 				setTimeout(function(){
-					select.removeClass(place)},[500]
+					select.removeClass(place);},[500]
 				);
-			}
-			addGoat(select,place)
-			removeGoat(select,place)
+			};
+			addGoat(select,place);
+			removeGoat(select,place);
+		};
+		// var addRandomGoatToPen = function(){
+		// 	var i=Math.floor(Math.random()*4);
+		// 	$goatPen.push($goatList[i]);
+		// };
+		var addGoatToPen = function(){
+			$goatPen.push($goatList[goatNumber])
 		}
-		var addRandomGoatToPen = function(){
-			var i=Math.floor(Math.random()*4);
-			$goatPen.push($goatList[i]);
-		}
-		addRandomGoatToPen();
-		var CompGoatSelector = function(){  
+		addGoatToPen();
+		var compGoatSelector = function(){  
 			for(computerClick;computerClick<$goatPen.length;computerClick++){
 				if ($goatPen[computerClick].selector==="#goatOne"){
 					addRemoveGoat($goatOne,'activateGoatOne');
@@ -42,36 +46,43 @@ $(document).ready(function() {
 					addRemoveGoat($goatThree,'activateGoatThree');
 				} else if ($goatPen[computerClick].selector==="#goatFour"){
 					addRemoveGoat($goatFour,'activateGoatFour');
-				};
-			};
-		}
-		setTimeout(function(){CompGoatSelector($goatPen[computerClick])},[500]);
+				}
+			}
+		};
+		setTimeout(function(){compGoatSelector($goatPen[computerClick]);},[500]);
 		var goatSelector = function(event){
 			if (event.target.id==='goatOne'){
 				addRemoveGoat($goatOne,'activateGoatOne');
 				$humanPen.push($goatOne);
 			} else if (event.target.id==='goatTwo'){
 				addRemoveGoat($goatTwo,'activateGoatTwo');
-				$humanPen.push($goatOne);				
+				$humanPen.push($goatTwo);				
 			} else if (event.target.id==='goatThree'){
 				addRemoveGoat($goatThree,'activateGoatThree');
 				$humanPen.push($goatThree);
 			} else if (event.target.id==='goatFour'){
 				addRemoveGoat($goatFour,'activateGoatFour');
 				$humanPen.push($goatFour);
-			};
-		humanClick++};	
+			}
+		humanClick++};
+
 		$allGoats.on('click',goatSelector);
 		var clickChecker = function(){
+			console.log($goatPen[counter].selector);
+			console.log($humanPen[counter].selector);
 			for(counter;counter<$goatPen.length;counter++){
 				if ($goatPen[counter].selector===$humanPen[counter].selector){
 					console.log('great job');
+					counter++
 				} else {
-					console.log('failure')
+					console.log('failure');
 				}
 			}
-		addRandomGoatToPen();}
-		$allGoats.on('click',clickChecker)
+		goatNumber++;
+		addGoatToPen()
+		setTimeout(function(){compGoatSelector($goatPen[computerClick]);},[500]);
+		};
+		$allGoats.on('click',clickChecker);
 	};
 	$start.on('click',startGame);
 });
