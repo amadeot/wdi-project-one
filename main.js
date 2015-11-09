cfbdbff93ba9c16db7c2f6f9897dceb0dab6079d
Max Thomas <maxamadeothomas@gmail.com>
2015-11-08 19:12:38 -0500

    figured out how to add sound, bit more stylin

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	var audio = new Audio('TheScreamingSheep.mp3');
 	console.log('linked!');
 	$allGoats = $('.goatButton');
 	$goatOne = $('#goatOne');
@@ -6,74 +7,80 @@ $(document).ready(function() {
 	$goatThree = $('#goatThree');
 	$goatFour = $('#goatFour');
 	$start = $('#start');
-
 	$goatList = [$goatOne,$goatTwo,$goatThree,$goatFour];
 	$goatPen = [];
 	$humanPen = []; 
 	counter=0;
 	computerClick=0;
-	goatNumber=0;
-
-	var startGame = function(event){	
-		var addRemoveGoat=function(select,place){
-			var addGoat = function(select,place){
+//all variables used above
+	var startGame = function(event){	//nothing starts until the startgame function runs
+		var addRemoveGoat=function(select,place){//this is the process to make a goat flash
+			var addGoat = function(select,place){//this is the part where it flashes on
 			select.addClass(place);
 		};
-			var removeGoat = function(select,place){
+			var removeGoat = function(select,place){//this is the part where it flashes off
 				setTimeout(function(){
 					select.removeClass(place);},[250]
 				);
 			};
-			addGoat(select,place);
+			addGoat(select,place);//calling functions in functions for this
 			removeGoat(select,place);
 		};
-		var addGoatToPen = function(){
+		var addGoatToPen = function(){//this is how to add a random goat to the list of goats to click
 			var goatNumber = Math.floor(Math.random()*4);
 			$goatPen.push($goatList[goatNumber])
 		}
-		addGoatToPen();
-		var compGoatSelector = function(){  
+		addGoatToPen();//callin it to start off the game
+		var compGoatSelector = function(){  //computer goes first, runs through list of goats in the pen
 			while(computerClick<$goatPen.length){
 				if ($goatPen[computerClick].selector==="#goatOne"){
 					addRemoveGoat($goatOne,'activateGoatOne');
+					audio.play();
 				} else if ($goatPen[computerClick].selector==="#goatTwo"){
 					addRemoveGoat($goatTwo,'activateGoatTwo');
+					audio.play();
 				} else if ($goatPen[computerClick].selector==="#goatThree"){
 					addRemoveGoat($goatThree,'activateGoatThree');
+					audio.play();
 				} else if ($goatPen[computerClick].selector==="#goatFour"){
 					addRemoveGoat($goatFour,'activateGoatFour');
+					audio.play();
 				}
 			computerClick++}
 			computerClick=0
 		};
-		setTimeout(function(){compGoatSelector($goatPen[computerClick]);},[500]);
+		setTimeout(function(){compGoatSelector($goatPen[computerClick]);},[500]);//callin it with a slight delay
 
-		var goatSelector = function(event){
+		var goatSelector = function(event){//this is how the human clicks work
 			if (event.target.id==='goatOne'){
 				addRemoveGoat($goatOne,'activateGoatOne');
 				$humanPen.push($goatOne);
+				audio.play();
 			} else if (event.target.id==='goatTwo'){
 				addRemoveGoat($goatTwo,'activateGoatTwo');
-				$humanPen.push($goatTwo);				
+				$humanPen.push($goatTwo);
+				audio.play();				
 			} else if (event.target.id==='goatThree'){
 				addRemoveGoat($goatThree,'activateGoatThree');
 				$humanPen.push($goatThree);
+				audio.play();
 			} else if (event.target.id==='goatFour'){
 				addRemoveGoat($goatFour,'activateGoatFour');
 				$humanPen.push($goatFour);
+				audio.play();
 			}
 		};
 
-		$allGoats.on('click',goatSelector);
-		var clickChecker = function(){
+		$allGoats.on('click',goatSelector);//callin it
+		var clickChecker = function(){//this checks the human clicks against the computer clicks
 			if($goatPen.length === $humanPen.length){
 				for(counter;counter<$humanPen.length;counter++){
 					if ($goatPen[counter].selector===$humanPen[counter].selector){
 						console.log('great job');
 						counter=0
+						addGoatToPen()
 						setTimeout(function(){compGoatSelector($goatPen[computerClick]);},[750]);
 						$humanPen=[]
-						addGoatToPen()
 					} else {
 						console.log('failure');
 					}
